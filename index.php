310199b70e99ffae6baa3295b342bf5d225f0fce
Emirhan Aluş <alusemirhan@gmail.com>
2023-01-11 17:49:54 +0300

    Cleaner Code

--- index.php
+++ index.php
@@ -10,34 +10,13 @@
     if(isset($_POST['submit']))
     {
         // Validate name
-        if (empty($_POST['name']))
-        {
-            $nameErr = 'Name is required.';
-        }
-        else
-        {
-            $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
-        }
+        empty($_POST['name']) ? $nameErr = 'Name is required.' : $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
 
         // Validate email
-        if (empty($_POST['email']))
-        {
-            $emailErr = 'Email is required.';
-        }
-        else
-        {
-            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-        }
+        empty($_POST['email']) ? $emailErr = 'Email is required.' : $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 
         // Validate body
-        if (empty($_POST['body']))
-        {
-            $bodyErr = 'Feedback is required.';
-        }
-        else
-        {
-            $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_SPECIAL_CHARS);
-        }
+        empty($_POST['body']) ? $bodyErr = 'Feedback is required.' : $body = filter_input(INPUT_POST, 'body', FILTER_SANITIZE_SPECIAL_CHARS);
 
         // Final check before INSERT.
         if(empty($nameErr) && empty($emailErr) && empty($bodyErr))
